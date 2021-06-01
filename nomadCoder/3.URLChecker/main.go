24dd86cb27cbac4e5ac53c49d29e8a5ee9e55329
HTaeha <patterson@linecare.net>
2021-06-01 15:24:17 +0900

    URL check testing

--- nomadCoder/3.URLChecker/main.go
+++ nomadCoder/3.URLChecker/main.go
@@ -4,58 +4,48 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"time"
 )
+
+type requestResult struct {
+	url    string
+	status string
+}
 
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	// results := map[string]string{}
-	// urls := []string{
-	// 	"https://www.airbnb.com/",
-	// 	"https://www.google.com/",
-	// 	"https://www.amazon.com/",
-	// 	"https://www.reddit.com/",
-	// 	"https://soundcloud.com/",
-	// 	"https://www.facebook.com/",
-	// 	"https://www.instagram.com/",
-	// 	"https://academy.nomadcoders.co/",
-	// }
+	c := make(chan requestResult)
+	results := make(map[string]string)
 
-	// for _, url := range urls {
-	// 	result := "OK"
-	// 	err := hitURL(url)
-	// 	if err != nil {
-	// 		result = "FAILED"
-	// 	}
-	// 	results[url] = result
-	// }
-	// for url, result := range results {
-	// 	fmt.Println(url, result)
-	// }
-
-	c := make(chan bool)
-	people := [2]string{"taeha", "nico"}
-	for _, person := range people {
-		go isSexy(person, c)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking: ", url)
+func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
-}
-
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- true
+	c <- requestResult{url: url, status: status}
 }
